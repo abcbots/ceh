058435f9e49396d5db6783513a829a51f20250f0
Dave Make <davemakena@gmail.com>
2017-03-23 13:36:39 -0700

    page1 ts add db as openDatabase

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ImagePicker } from 'ionic-native';
-import { NavController } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
+
+declare var window: any;
 
 @Component({
         selector: 'page-page1',
@@ -11,24 +13,35 @@ export class Page1 {
 
 	imagepicker: any;
 	images: any = [];
+	db: any;
 
 	constructor(
-		public navCtrl: NavController
+		public platform: Platform
 	) {
 		this.imagepicker = ImagePicker;
+		if (window.cordova) {
+			this.imagepicker = ImagePicker;
+		} else {
+			if (window.openDatabase)
+				this.db = window.openDatabase;
+			};
+		};
         }
 
-	add() {
+	pick() {
+
 		let options = {
 			width: 800,
 			height: 600,
 			maximumImagesCount: 10
 		};
+
 		this.imagepicker.getPictures(options).then( (data)=> {
 			for (let i=0;data.length>i;i++) {
 				this.images.push(data[i]);
 			};
 		});
+
 	}
 
 }
